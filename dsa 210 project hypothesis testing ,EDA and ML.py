89e4f1c7354e1acc922fd7804610c141a8976eea
yasinberkaynarin <berkay.narin@sabanciuniv.edu>
2026-05-05 14:27:52 +0300

    Update and rename dsa 210 project hypothesis testing and EDA.py to dsa 210 project hypothesis testing ,EDA and ML.py

--- dsa 210 project hypothesis testing ,EDA and ML.py
+++ dsa 210 project hypothesis testing ,EDA and ML.py
@@ -58,3 +58,27 @@ if p_value < 0.05:
 else:
     print("Fail to Reject H0: No significant difference found.")
 
+# MACHINE LEARNING
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+X = df[['budget','score','runtime']]
+y = df['votes']
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print("\n===== Machine Learning Result =====")
+print("MSE:", mse)
+print("R2 Score:", r2)
+plt.figure(figsize=(6,6))
+plt.scatter(y_test, y_pred, alpha=0.6)
+plt.xlabel("Actual Votes")
+plt.ylabel("Predicted Votes")
+plt.title("Actual vs Predicted Popularity")
+plt.show()
